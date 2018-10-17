fbf7a75e43e63943d8ba46c46d3a8dbcfa918fe3
Adnan Usman <adnantu@hotmail.com>
2018-10-16 17:00:07 -0700

    when two cards are flipped, they just flip back to the front

--- build/js/app.js
+++ build/js/app.js
@@ -1,18 +1,38 @@
 var matchTwoApp = {
+  activeCards: [],
   init: function() {
     this.cardFrontEventListeners();
     this.cardBackEventListeners();
   },
+  // Setup Event Listeners for the front side of the card.
   cardFrontEventListeners: function() {
     var cardfronts = document.querySelectorAll('.card-front');
- 
+    var app = this;
+
     for(i = 0; i < cardfronts.length; i++) {
       cardfronts[i].addEventListener('click', function(e) {
         var cardFront = e.target;
         var cardBack = e.target.previousElementSibling;
-    
+        
+        app.activeCards.push({cardFront, cardBack});
+        // Flip the cards
         cardFront.style.transform = 'perspective(600px) rotateY(-180deg)';
         cardBack.style.transform = 'perspective(600px) rotateY(0deg)';
+
+        if(app.activeCards.length === 2) {
+          setTimeout(resetCards, 1000);
+          
+          function resetCards() {
+            app.activeCards.forEach(card => {
+              card.cardBack.style.transform = '';
+              card.cardFront.style.transform = '';
+
+              app.activeCards = [];            
+            })
+          }
+        }
+
+
       })
     } 
   },
@@ -24,7 +44,8 @@ var matchTwoApp = {
         console.log(e);
         var cardBack = e.target;
         var cardFront = e.target.nextElementSibling;
-    
+        
+        // Flip the cards back to normal state.
         cardFront.style.transform = 'perspective(600px) rotateY(0deg)';
         cardBack.style.transform = 'perspective(600px) rotateY(180deg)';
       })
